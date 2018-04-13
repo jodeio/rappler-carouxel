7ab71af33659245be8f30928b6e974b809e76f1f
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-04-14 02:06:05 +0800

    Update comment stubs

--- src/demo/App.js
+++ src/demo/App.js
@@ -4,8 +4,10 @@ import RapplerCarouxel from '../lib';
 export default class App extends React.Component {
   render() {
     const carouxel = {
-      // For api, please refer to api changes at docs www.url.com here
+      // For api, please refer to api changes at docs www.github.com/jodeio/rappler-carouxel#readme.com here
       api: "http://svc.rappler.com/p/topstories",
+      
+      // Here you can replace the settings and behavior of the base slider, docs available at https://react-slick.neostack.com/docs/example/
       // settings: {
       //   autoplay: true,
       //   dots: true,
