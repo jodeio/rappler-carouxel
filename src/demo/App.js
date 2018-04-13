bb7e640091a15733bdbe8c6bbe369a3e80274ad8
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2018-04-14 00:30:14 +0800

    Add/Setup demo component call

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import RapplerCarouxel from '../lib';
 
-const App = () => (
-  <div>
-    <RapplerCarouxel />
-  </div>
-);
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    const carouxel = {
+      // For api, please refer to api changes at docs www.url.com here
+      api: "http://svc.rappler.com/p/topstories",
+      settings: {
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 100,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    };
+    return (
+      <div>
+        <RapplerCarouxel {...carouxel}/>
+      </div>
+    );
+  }
+}
